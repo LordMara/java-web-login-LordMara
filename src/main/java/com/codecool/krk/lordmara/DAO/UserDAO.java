a04e7bba734b80b456fb45d672788cc144eef176
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-11-18 22:27:43 +0100

    Cahnge implelemtation of getUser() method, add field PATH and createPreparedStatement() method

--- src/main/java/com/codecool/krk/lordmara/DAO/UserDAO.java
+++ src/main/java/com/codecool/krk/lordmara/DAO/UserDAO.java
@@ -2,12 +2,12 @@ package com.codecool.krk.lordmara.DAO;
 
 import com.codecool.krk.lordmara.model.User;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class UserDAO {
     private static final UserDAO INSTANCE = new UserDAO();
+
+    private final String PATH = "jdbc:sqlite:src/main/resources/db/table_reservation_system.db";
 
     public static UserDAO getInstance() {
         return INSTANCE;
@@ -16,25 +16,28 @@ public class UserDAO {
     public User getUser(String enteredLogin, String enteredPassword) throws SQLException {
         User user = null;
 
+        try (Connection con = DriverManager.getConnection(this.PATH);
+        PreparedStatement ps = createPreparedStatement(con, enteredLogin, enteredPassword);
+        ResultSet result = ps.executeQuery()) {
+
+            while (result.next()) {
+                Integer id = result.getInt("id");
+                String name = result.getString("name");
+                String surname = result.getString("surname");
+
+                user = new User(id, name, surname, enteredLogin, enteredPassword);
+            }
+        }
+        return user;
+    }
+
+    private PreparedStatement createPreparedStatement(Connection con, String enteredLogin, String enteredPassword) throws SQLException {
         String query = "SELECT id, name, surname FROM user WHERE login = ? AND password = ?";
 
-        PreparedStatement pstmt = DatabaseConnection.getConnection().prepareStatement(query);
-        pstmt.setString(1, enteredLogin);
-        pstmt.setString(2, enteredPassword);
+        PreparedStatement ps = con.prepareStatement(query);
+        ps.setString(1, enteredLogin);
+        ps.setString(2, enteredPassword);
 
-        ResultSet result = pstmt.executeQuery();
-
-        while (result.next()) {
-            Integer id = result.getInt("id");
-            String name = result.getString("name");
-            String surname = result.getString("surname");
-
-            user = new User(id, name, surname, enteredLogin, enteredPassword);
-        }
-
-        result.close();
-        pstmt.close();
-
-        return user;
+        return ps;
     }
 }
