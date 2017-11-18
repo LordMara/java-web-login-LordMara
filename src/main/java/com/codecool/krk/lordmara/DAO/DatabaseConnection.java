6ade4bd91f6f6b849211d63f694a076cad409969
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-11-18 22:41:21 +0100

    Implement class DatabaseConnection with getConnection() method

--- src/main/java/com/codecool/krk/lordmara/DAO/DatabaseConnection.java
+++ src/main/java/com/codecool/krk/lordmara/DAO/DatabaseConnection.java
@@ -5,13 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static Connection connection = null;
-
     public static Connection getConnection() throws SQLException{
-        if (connection == null) {
-            connection = DriverManager.getConnection("jdbc:sqlite:src/main/resources/db/table_reservation_system.db");
-        }
-
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:src/main/resources/db/table_reservation_system.db");
         return connection;
     }
 }
