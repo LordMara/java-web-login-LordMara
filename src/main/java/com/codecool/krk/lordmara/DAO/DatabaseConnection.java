510ee8b3d45d212a8abe659aac068748a5d6c858
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-11-18 23:22:03 +0100

    Fix wrong path to database

--- src/main/java/com/codecool/krk/lordmara/DAO/DatabaseConnection.java
+++ src/main/java/com/codecool/krk/lordmara/DAO/DatabaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     public static Connection getConnection() throws SQLException{
-        Connection connection = DriverManager.getConnection("jdbc:sqlite:src/main/resources/db/table_reservation_system.db");
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:src/main/resources/db/database.db");
         return connection;
     }
 }
