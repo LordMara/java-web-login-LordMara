a7543536f06cd810c0392bd143f04542fc27a33f
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-11-21 12:51:43 +0100

    Overload getUser() and createPreparedStatement() methods

--- src/main/java/com/codecool/krk/lordmara/DAO/UserDAO.java
+++ src/main/java/com/codecool/krk/lordmara/DAO/UserDAO.java
@@ -33,12 +33,44 @@ public class UserDAO {
         return user;
     }
 
+    public User getUser(Integer userId) {
+        User user = null;
+
+        try (Connection con = DatabaseConnection.getConnection();
+             PreparedStatement ps = createPreparedStatement(con, userId);
+             ResultSet result = ps.executeQuery()) {
+
+            while (result.next()) {
+                String name = result.getString("name");
+                String surname = result.getString("surname");
+                String user_name = result.getString("user_name");
+                String password = result.getString("password");
+
+                user = new User(userId, name, surname, user_name, password);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+
+        return user;
+    }
+
     private PreparedStatement createPreparedStatement(Connection con, String enteredUserName, String enteredPassword) throws SQLException {
-        String query = "SELECT id, name, surname FROM user WHERE user_name = ? AND password = ?";
+        String query = "SELECT id, name, surname FROM user WHERE user_name = ? AND password = ?;";
 
         PreparedStatement ps = con.prepareStatement(query);
         ps.setString(1, enteredUserName);
         ps.setString(2, enteredPassword);
+
+        return ps;
+    }
+
+    private PreparedStatement createPreparedStatement(Connection con, Integer userId) throws SQLException {
+        String query = "SELECT name, surname, user_name, password FROM user WHERE id = ?;";
+
+        PreparedStatement ps = con.prepareStatement(query);
+        ps.setInt(1, userId);
 
         return ps;
     }
